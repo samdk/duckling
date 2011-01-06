b0a41cc79e2d62dec99f9661532e9208e740cbfb
Sam DeFabbia-Kane <sam@defabbiakane.com>
2011-01-05 20:56:00 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Duckling::Application.config.secret_token = '22606f03c8b6a137a4b85fedb0325b5e8ec6bc9ee1a2a5ab6938c6f532cbd64eedaad487e05a0bce8f119266c457832e22f2ee381b74548b8e68c5e0dc4f43d5'
