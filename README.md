57b19c9c22e14b4845e36a98ffd3f95e1b38448a
Eli <eli@elilies.com>
2011-01-07 00:51:33 -0500

    Start with some cleaning

---
+++ README.md
@@ -0,0 +1,7 @@
+# Duckling Overview
+
+# Philosophy
+
+# Installation
+
+# Contributing
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,7 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Duckling::Application.config.secret_token = '22606f03c8b6a137a4b85fedb0325b5e8ec6bc9ee1a2a5ab6938c6f532cbd64eedaad487e05a0bce8f119266c457832e22f2ee381b74548b8e68c5e0dc4f43d5'
+
+keys = YAML.load_file(File.expand_path('../keys.yml', __FILE__))
+
+Duckling::Application.config.secret_token = keys.keys.secret_token
