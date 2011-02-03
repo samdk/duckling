0c0421b94905c82c325ab1fdd43393ae942f40dd
Eli <eli@elilies.com>
2011-02-02 23:31:43 -0500

    Rearrange static config

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -1,11 +1,11 @@
-APP_CONFIG = YAML.load_file(File.expand_path('../keys.yml', __FILE__))[ENV['RAILS_ENV']]
+APP_CONFIG = YAML.load_file(File.expand_path('../config.yml', __FILE__))[ENV['RAILS_ENV']]
 
 Duckling::Application.config.secret_token = APP_CONFIG['secret_token']
 
 FILE_STORAGE_OPTS = if s3 = APP_CONFIG['s3']
   { secret_access_key: s3['secret_access'],
     access_key_id:     s3['access_key_id'],
-    s3_protocol:       'https'} # we need more here
+    s3_protocol:       'https' } # we need more here
 else
   { path: ':rails_root/attachments/:attachment/:id/:filename' }
 end
