353922ae1b9eed4473f1d727f62e9dd5762952be
Eli <eli@elilies.com>
2011-02-06 20:07:09 -0500

    Updating routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Duckling::Application.routes.draw do
   
   resources :people, controller: 'users'
-  resources :sessions
-
+  
+  match '/login' => 'sessions#new', :via => :get, :as => :login
+  match '/logout' => 'sessions#destroy', :as => :logout
+  
+  resource :account, controller: 'users' do
+    member do
+       get :activate
+       get :forgot_password
+      post :reset_password
+    end
+  end
+  
   resources :activations do
     resources :updates    
     resources :people, controller: 'users'
