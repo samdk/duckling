38a765f962075a9d752228af1647d341be394994
Sam DeFabbia-Kane <sam@defabbiakane.com>
2011-03-18 12:22:01 -0400

    Got basic logins working.

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,12 +1,22 @@
-class SessionController < ApplicationController
+class SessionController < AuthorizedController 
   
+  skip_login({:only => [:new, :create]})
+
   def new
   end
 
   def destroy
+    log_out!
   end
 
   def create
+    u = User.with_credentials(params[:email],params[:password])
+    if u
+      log_in_as(u)
+      redirect_to '/'
+    else
+      redirect_to :back
+    end
   end
 
 end
