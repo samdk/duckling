5a3576e93df21e49d67d44dd7520ca87709d43f9
Sam DeFabbia-Kane <sam@defabbiakane.com>
2011-03-18 14:42:40 -0400

    Changed 'Update.user' to 'Update.author'
    Author is more clear about the relationship between the user/update than
    user is.

--- app/models/update.rb
+++ app/models/update.rb
@@ -1,10 +1,14 @@
 class Update < ActiveRecord::Base
-  belongs_to :user
+  belongs_to :author, class_name: 'User'
   belongs_to :activation, counter_cache: true
   
   has_many :comments
   has_many :file_uploads
   
+  # TODO: change/remove upper limits on these?
+  # there's nothing more obnoxious than poorly-defined,
+  # arbitary, unnecessary limits on the amount of text
+  # you're allowed to enter into a field
   validates :title, presence: true, length: { within: 2..50 }
   validates :body, presence: true, length: { within: 2..10000 }
 end
