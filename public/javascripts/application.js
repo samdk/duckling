99a6d47f007b233a422519ce4ef3f0e5e88fa10f
Sam DeFabbia-Kane <sam@defabbiakane.com>
2011-06-04 18:21:57 -0600

    style changes to users page, some js for filters

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,39 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(function(){
+  toggle($('#multiselect-groups a'),$('#multiselect-groups ul'));
+  defaultText($('#search_query'),'Search');
+});
+
+/*
+ * utility functions that encompass common patterns
+ */
+
+// clicking on the element 'button' toggles the display
+// of the element 'elem'. 'elem' hidden by default.
+function toggle(button,elem){
+  button.click(function(e){
+    e.preventDefault();
+    if (elem.css('display') === 'none') elem.show();
+    else                                elem.hide();
+  });
+};
+
+// hides default text in a text input when clicked on
+// and makes sure default text doesn't get submitted
+function defaultText(textInput,text){
+  function hideText(){
+    if (textInput.val() === text) {
+      textInput.val('');
+      textInput.removeClass('default-text');
+    }
+  };
+  function showText(){
+    if (textInput.val() === '') {
+      textInput.val(text);
+      textInput.addClass('default-text');
+    };
+  };
+  showText(); // this gets called once at start to set text
+  textInput.click(hideText).blur(showText);
+  textInput.parents('form').submit(hideText);
+}
+
