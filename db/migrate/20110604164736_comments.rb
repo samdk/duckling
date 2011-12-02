b835f27f689bd8c61dd5c5d46b9b1c006d3936d9
Eli <eli@elilies.com>
2011-12-02 11:40:39 -0500

    Fix migration

--- db/migrate/20110604164736_comments.rb
+++ db/migrate/20110604164736_comments.rb
@@ -1,7 +1,7 @@
 class Comments < ActiveRecord::Migration
   def self.up
     create_table 'comments', force: true do |t|
-      t.text     'body',       limit: 5_000
+      t.text     'body'
       t.integer  'author_id'
       t.integer  'update_id'
       t.datetime 'created_at'
