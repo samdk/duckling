cc98c50cd9a27072fe1c693f2386f1345df718cf
Eli <eli@elilies.com>
2012-01-13 11:31:41 -0500

    Fix up tabs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,15 +8,19 @@ module ApplicationHelper
     link_to "/attachment/#{upload.inspect}", upload.upload_file_name
   end
   
-  def current_scope?(arg)
-    if Hash === arg
-      (!arg.key?(:controller) or arg[:controller] == controller_name) and (!arg.key?(:action) or arg[:action] == action_name)
-    else
-      current_page? arg
+  def current_scope?(*args)
+    args.any? do |arg|
+      if Hash === arg
+        (!arg.key?(:controller) or arg[:controller] == controller_name) and (!arg.key?(:action) or arg[:action] == action_name)
+      else
+        current_page? arg
+      end
     end
   end
   
-  def active?(arg)
-    current_scope?(arg) ? 'active' : 'inactive'
+  def active(arg)
+    arg = current_scope?(arg) unless true === arg or false === arg
+    arg ? 'active' : 'inactive'
   end
+  
 end
