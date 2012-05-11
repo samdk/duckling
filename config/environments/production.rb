4d32b401a2c141529d14b37ab6d865e4c4b711b4
Eli <eli@elilies.com>
2012-05-11 15:05:06 -0400

    Faster pre-compiling

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Duckling::Application.configure do
   config.cache_classes = true
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
 
   config.action_dispatch.x_sendfile_header = "X-Sendfile"
@@ -8,7 +8,10 @@ Duckling::Application.configure do
   
   config.serve_static_assets = false
   # config.action_controller.asset_host = "http://assets.example.com"
+  config.assets.initialize_on_precompile = false
+  
   # config.action_mailer.raise_delivery_errors = false
   config.i18n.fallbacks = true
+
   config.active_support.deprecation = :notify
 end
