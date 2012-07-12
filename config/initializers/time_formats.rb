95876f2882702fb74b6f8581bdf78914501e11b5
Eli <eli@elilies.com>
2012-07-11 23:28:52 -0400

    save state

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,8 +1,8 @@
 formats = {
   time: "%l:%M %p",           # hour:minute am/pm
   mdy:  "%m/%d/%y",           # month/day/year
-  date: "%A, %B %d",          # day name, month name, day
-  date_year: "%A, %B %d (%Y)" # day name, month name, day (year)
+  date: "%A, %B %-d",          # day name, month name, day
+  date_year: "%A, %B %-d (%Y)" # day name, month name, day (year)
 }
 
 formats.each do |k,v|
