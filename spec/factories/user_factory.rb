48acbc21a9a71218187d2a8a5cdd182d3508fdf2
Cameron Cundiff <ckundo@gmail.com>
2012-07-14 12:06:29 -0400

    Adding User and Activation factories.

---
+++ spec/factories/user_factory.rb
@@ -0,0 +1,13 @@
+FactoryGirl.define do
+  factory :user do
+    first_name Faker::Name.first_name
+    last_name  Faker::Name.last_name
+    password 'foobarbaz'
+    password_confirmation 'foobarbaz'
+
+    after(:create) do |user, evaluator|
+      user.authorize_with(user).add_email('#{evaluator.first_name}.#{evaluator.last_name}@example.com')
+      user.primary_email.activate
+    end
+  end
+end
