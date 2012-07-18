ad8be039409d33d8d49802ae83807d9fae566adc
Eli <eli@elilies.com>
2012-07-18 19:42:42 -0400

    More resque stuff

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -3,7 +3,15 @@ class Invitation < ActiveRecord::Base
   belongs_to :invitable, polymorphic: true
   belongs_to :inviter, class_name: 'User'
   
+  def after_create
+    async.deliver
+  end
+  
   def interested_emails
     [email]
+  end
+  
+  def deliver
+    UserMailer.invite(self).deliver
   end
 end
--- lib/tifi_tasks/invitations.rb
+++
@@ -1,9 +0,0 @@
-module TifiTasks
-  class Invitations
-    def perform
-      if i = Invitation.where(emailed: false).first
-        UserMailer.invite(i).deliver
-      end
-    end
-  end
-end
