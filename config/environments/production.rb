a8620b8b0e109c16928ec05ba6c5edb7c42ad41c
Eli <eli@elilies.com>
2012-07-22 17:18:47 -0400

    Fix email for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,8 +9,19 @@ Duckling::Application.configure do
   config.serve_static_assets = false
   # config.action_controller.asset_host = "http://assets.example.com"
   
-  # config.action_mailer.raise_delivery_errors = false
   config.i18n.fallbacks = true
 
   config.active_support.deprecation = :notify
+  
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: 'duckling.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { address:              'email-smtp.us-east-1.amazonaws.com',
+                                         port:                 465,
+                                         user_name:            'AKIAI7EFTJY4RVB5PT4A',
+                                         password:             'AntZ1wov69YqFRyLIh3uyjjCWnNmJiuqmAc7tFqihIoz',
+                                         domain:               'clbt.net',
+                                         authentication:       'plain',
+                                         enable_starttls_auto: true }
+  
 end
